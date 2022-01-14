ce9466507e8cb951b0597b8c4921cccfb5e7fa78
Tobias Schnabel <toschnab@microsoft.com>
2022-01-14 11:52:07 -0800

    removed support for versions Python 3.5 and below

--- setup.py
+++ setup.py
@@ -27,7 +27,7 @@ setup(
     packages=find_packages(exclude=('tests', 'docs', 'test')),
 
     install_requires=['numpy>=1.18', 'numba>=0.53.0', 'scipy>=1.0'],
-    python_requires='>=3',
+    python_requires='>=3.6',
 
     classifiers=[
         'Development Status :: 4 - Beta',
